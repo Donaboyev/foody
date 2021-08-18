81da64cdda6be558baa5cfc552ebb21fd8790ad1
Donaboyev <donaboyevabbos1701@gmail.com>
2021-08-18 17:46:55 +0500

    base url and api key added

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,6 +2,8 @@ class AppConstants {
   static const VERSION = String.fromEnvironment('VERSION');
   static const Version RUNNING =
       VERSION == 'lazy' ? Version.LAZY : Version.WAIT;
+  static const BASE_URL = 'https://api.spoonacular.com';
+  static const API_KEY = 'YOUR_API_KEY';
 }
 
 enum Version {
